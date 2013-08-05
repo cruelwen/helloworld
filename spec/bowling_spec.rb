091288de4b5fc424555968f7cf2f7f7d6a18084d
cruelwen <wenli@baidu.com>
2013-08-05 15:08:24 +0800

    add travis

---
+++ spec/bowling_spec.rb
@@ -0,0 +1,19 @@
+# bowling_spec.rb
+
+$:.unshift(File.expand_path("../../", __FILE__))
+require 'bowling'
+
+describe Bowling, "#score" do
+  it "returns 0 for all gutter game" do
+    bowling = Bowling.new
+    20.times { bowling.hit(0) }
+    bowling.score.should eq(0)
+  end
+end
+
+describe Bowling, "#hit" do
+  it "returns 0 for all gutter game" do
+    bowling = Bowling.new
+    bowling.hit(0).should == 0
+  end
+end
